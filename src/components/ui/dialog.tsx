0b2cde8c9df8ac0a23433362fbd1268d6598e082
truonghq <huynhquoctruongit@gmail.com>
2026-05-26 00:35:07 +0700

    fix: sửa Tailwind calc syntax – dùng underscore cho space

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -31,7 +31,7 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        'fixed left-[50%] top-[50%] z-50 grid w-[calc(100%-2rem)] max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg rounded-lg',
+        'fixed left-[50%] top-[50%] z-50 grid w-[calc(100%_-_2rem)] max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg rounded-lg',
         className,
       )}
       {...props}
